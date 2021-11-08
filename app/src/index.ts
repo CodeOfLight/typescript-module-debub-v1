910f9b7ea24310ffd2af4d9a2163ea319b0f8e66
Code of Light <warrick.fitzgerald+codeoflight@gmail.com>
2021-11-08 13:25:56 -0500

    vs code tasks to build webpack before runnign the webpack debugger

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,4 +1,4 @@
 import {mathTools} from '@testlibs/helpers'
-console.log('Start ....')
+console.log('Start yyy....')
 var result = mathTools.addOne(4)
 console.log("The result of adding 1 to 4 was :" + result)
